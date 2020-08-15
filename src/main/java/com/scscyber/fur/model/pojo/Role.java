06f8574ed9a73db75a127a48f7bb6529a5fcb2d9
Phan Thanh Tuan <tuan.phanth91@gmail.com>
2020-08-15 19:37:30 +0700

    Master (#3)
    * set build executable
    
    * add role endpoint

---
+++ src/main/java/com/scscyber/fur/model/pojo/Role.java
@@ -0,0 +1,15 @@
+package com.scscyber.fur.model.pojo;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class Role {
+    @NotBlank(message = "Role ID could not be blank.")
+    private int roleId;
+    @NotBlank(message = "Role Name could not be blank.")
+    private String roleName;
+    private String roleDescription;
+
+}
