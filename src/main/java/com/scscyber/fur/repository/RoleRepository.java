d800769cbefef10612146eedf1641741560869be
Phan Thanh Tuan <tuan.phanth91@gmail.com>
2020-08-16 20:41:58 +0700

    Master (#5) (#7)
    * set build executable
    
    * add role endpoint
    
    * update library

---
+++ src/main/java/com/scscyber/fur/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.scscyber.fur.repository;
+
+import com.scscyber.fur.model.pojo.Role;
+
+import java.util.List;
+
+public interface RoleRepository {
+    List<Role> getAllRole();
+    Role getRoleById(int id);
+    Role getRoleByName(String roleName);
+
+}
