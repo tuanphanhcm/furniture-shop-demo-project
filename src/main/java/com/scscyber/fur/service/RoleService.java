a0d594cfe592395cc1e798678371f30f1ad49bf0
Phan Thanh Tuan <tuan.phanth91@gmail.com>
2020-08-16 09:47:51 +0700

    update library (#6)
    * set build executable (#1)
    
    * Master (#3)
    
    * set build executable
    
    * add role endpoint
    
    * Master (#5)
    
    * set build executable
    
    * add role endpoint
    
    * update library

---
+++ src/main/java/com/scscyber/fur/service/RoleService.java
@@ -0,0 +1,35 @@
+package com.scscyber.fur.service;
+
+import com.scscyber.fur.model.pojo.Role;
+import com.scscyber.fur.mybatis.mapper.RoleMapper;
+import com.scscyber.fur.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RoleService implements IRoleService {
+
+    @Autowired
+    private final RoleMapper roleMapper;
+
+    public RoleService(RoleMapper roleMapper) {
+        this.roleMapper = roleMapper;
+    }
+
+    @Override
+    public List<Role> getAllRole() {
+        return roleMapper.getAllRole();
+    }
+
+    @Override
+    public Role getRoleById(int id) {
+        return roleMapper.getRoleById(id);
+    }
+
+    @Override
+    public Role getRoleByName(String roleName) {
+        return roleMapper.getRoleByName(roleName);
+    }
+}
